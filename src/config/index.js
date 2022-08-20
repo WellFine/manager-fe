35d7675ee3e2fe90e0a372281072340b79df45d6
unknown <1339531047@qq.com>
2022-08-20 17:58:41 +0800

    feat: 环境配置

---
+++ src/config/index.js
@@ -0,0 +1,21 @@
+const env = import.meta.env.MODE || 'production'  // 给一个线上的默认值，防止上线时出现意外
+const EnvConfig = {
+  development: {
+    baseApi: '/',
+    mockApi: 'https://www.fastmock.site/mock/c5cee191a657c9f7734bad9a0d6a03be/api'
+  },
+  test: {
+    baseApi: '//test.futurefe.com/api',  // url 是举例说明
+    mockApi: 'https://www.fastmock.site/mock/c5cee191a657c9f7734bad9a0d6a03be/api'
+  },
+  production: {
+    baseApi: '//futurefe.com/api',  // 举例说明
+    mockApi: ''
+  }
+}
+
+export default {
+  env,
+  mock: true,  // 是否用 mock 接口
+  ...EnvConfig[env]
+}
--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import axios from 'axios'
+import config from './config'
+
+axios.get(config.mockApi + '/login').then(console.log)
 
 const app = createApp(App)
 app.use(router)  // 引入 router 并挂载到 app 实例上
