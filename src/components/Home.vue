4d05a70324fbe55fa4947378105d3a19ce0bac31
unknown <1339531047@qq.com>
2022-08-20 15:18:50 +0800

    feat: 路由封装（上）

---
+++ src/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>欢迎学习Vue3全栈课程</h1>
+  <!-- 这个 router-view 用于显示 Home 组件路由的子路由组件，如 '/welcome' 对应的 Welcome 组件 -->
+  <router-view></router-view>
+</template>
+
+<script>
+  export default {
+    name: 'Home'
+  }
+</script>
+
+<style></style>
---
+++ src/components/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>欢迎来到登录界面</h1>
+  <el-button @click="goHome">回首页</el-button>
+</template>
+
+<!-- Componsition API 方式使用 router 跳转
+<script setup>
+  import { useRouter } from 'vue-router'
+  const router = useRouter()
+  const goHome = () => {
+    router.push('/welcome')
+  }
+</script>
+-->
+
+<script>
+  // Options API 方式使用 router 跳转
+  export default {
+    name: 'Login',
+    methods: {
+      goHome () {
+        this.$router.push('/welcome')
+      }
+    }
+  }
+</script>
+
+<style></style>
