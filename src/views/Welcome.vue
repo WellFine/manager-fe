6eac8fa9968471bbe3c68ab9e4b01e29e4e8cc09
unknown <1339531047@qq.com>
2022-08-29 00:49:19 +0800

    feat: 欢迎页面实现

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,6 +1,12 @@
 <template>
-  <h1>欢迎来到主界面</h1>
-  <router-link to="/login">去登录</router-link>
+  <div class="welcome">
+    <div class="content">
+      <div class="sub-title">欢迎体验</div>
+      <div class="title">慕课通用后台管理系统</div>
+      <div class="desc">- Vue3.0 + ElementPlus + Node + Mongo 打造通用后台管理系统</div>
+    </div>
+    <div class="img"></div>
+  </div>
 </template>
 
 <script>
@@ -9,4 +15,37 @@
   }
 </script>
 
-<style></style>
+<style lang="scss">
+  .welcome {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    .content {
+      position: relative;
+      bottom: 40px;
+      .sub-title {
+        font-size: 30px;
+        line-height: 40px;
+        color: #333;
+      }
+      .title {
+        font-size: 40px;
+        line-height: 62px;
+        color: #409eff;
+      }
+      .desc {
+        // text-align: right;
+        font-size: 14px;
+        color: #999;
+      }
+    }
+    .img {
+      background-image: url('../assets/images/welcome.png');
+      // 宽高保持与图片实际宽高一致
+      width: 371px;
+      height: 438px;
+      margin-left: 105px;
+    }
+  }
+</style>
